83cb8ecbb1651648840f83c6203df064bf355c4c
Anthony Hrvojic <1017633+anthfu@users.noreply.github.com>
2021-08-02 13:18:19 -0400

    Added `accept` function.

--- src/main/scala/com/anthfu/nio/AntoNIO.scala
+++ src/main/scala/com/anthfu/nio/AntoNIO.scala
@@ -1,5 +1,20 @@
 package com.anthfu.nio
 
-object AntoNIO {
+import java.nio.channels.{AsynchronousServerSocketChannel, AsynchronousSocketChannel, CompletionHandler}
+import scala.concurrent.{Future, Promise}
 
+object AntoNIO {
+  def accept[A](server: AsynchronousServerSocketChannel): Future[AsynchronousSocketChannel] = {
+    val p = Promise[AsynchronousSocketChannel]()
+
+    server.accept(null, new CompletionHandler[AsynchronousSocketChannel, Void]() {
+      override def completed(result: AsynchronousSocketChannel, attachment: Void): Unit =
+        Promise.successful(result)
+
+      override def failed(exc: Throwable, attachment: Void): Unit =
+        Promise.failed(exc)
+    })
+
+    p.future
+  }
 }
