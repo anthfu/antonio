88e18c62eaf208214174173f07582b2fc157411f
Anthony Hrvojic <1017633+anthfu@users.noreply.github.com>
2021-08-02 21:03:52 -0400

    Added client function.

--- src/main/scala/com/anthfu/nio/AntoNIO.scala
+++ src/main/scala/com/anthfu/nio/AntoNIO.scala
@@ -1,23 +1,41 @@
 package com.anthfu.nio
 
 import java.net.InetSocketAddress
-import java.nio.channels.{
-  AsynchronousChannelGroup,
-  AsynchronousServerSocketChannel,
-  AsynchronousSocketChannel,
-  CompletionHandler
-}
+import java.nio.channels.{AsynchronousChannelGroup, AsynchronousServerSocketChannel, AsynchronousSocketChannel, CompletionHandler}
 import scala.concurrent.{Future, Promise}
 
 object AntoNIO {
   def server(
     host: String,
     port: Int,
-    group: Option[AsynchronousChannelGroup] = None
+    channelGroup: Option[AsynchronousChannelGroup] = None
   ): AsynchronousServerSocketChannel = {
-    val server = AsynchronousServerSocketChannel.open(group.orNull)
+    val server = AsynchronousServerSocketChannel.open(channelGroup.orNull)
     server.bind(new InetSocketAddress(host, port))
     server
+  }
+
+  def client(
+    host: String,
+    port: Int,
+    channelGroup: Option[AsynchronousChannelGroup] = None
+  ): Future[AsynchronousSocketChannel] = {
+    val p = Promise[AsynchronousSocketChannel]()
+
+    val client = AsynchronousSocketChannel.open(channelGroup.orNull)
+    client.connect(
+      new InetSocketAddress(host, port),
+      null,
+      new CompletionHandler[Void, Void]() {
+        override def completed(result: Void, attachment: Void): Unit =
+          Promise.successful(client)
+
+        override def failed(e: Throwable, attachment: Void): Unit =
+          Promise.failed(e)
+      }
+    )
+
+    p.future
   }
 
   def accept(server: AsynchronousServerSocketChannel): Future[AsynchronousSocketChannel] = {
