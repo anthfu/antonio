c81a25068137a112b8bd0aca0312b1971561e95a
Anthony Hrvojic <1017633+anthfu@users.noreply.github.com>
2021-08-02 20:27:34 -0400

    AntoNIO WIP. Added Scalafmt configuration.

--- src/main/scala/com/anthfu/nio/AntoNIO.scala
+++ src/main/scala/com/anthfu/nio/AntoNIO.scala
@@ -1,18 +1,25 @@
 package com.anthfu.nio
 
-import java.nio.channels.{AsynchronousServerSocketChannel, AsynchronousSocketChannel, CompletionHandler}
+import java.net.InetSocketAddress
+import java.nio.channels.{AsynchronousChannelGroup, AsynchronousServerSocketChannel, AsynchronousSocketChannel, CompletionHandler}
 import scala.concurrent.{Future, Promise}
 
 object AntoNIO {
-  def accept[A](server: AsynchronousServerSocketChannel): Future[AsynchronousSocketChannel] = {
+  def server(host: String, port: Int, group: Option[AsynchronousChannelGroup] = None): AsynchronousServerSocketChannel = {
+    val server = AsynchronousServerSocketChannel.open(group.orNull)
+    server.bind(new InetSocketAddress(host, port))
+    server
+  }
+
+  def accept(server: AsynchronousServerSocketChannel): Future[AsynchronousSocketChannel] = {
     val p = Promise[AsynchronousSocketChannel]()
 
     server.accept(null, new CompletionHandler[AsynchronousSocketChannel, Void]() {
-      override def completed(result: AsynchronousSocketChannel, attachment: Void): Unit =
-        Promise.successful(result)
+      override def completed(channel: AsynchronousSocketChannel, attachment: Void): Unit =
+        Promise.successful(channel)
 
-      override def failed(exc: Throwable, attachment: Void): Unit =
-        Promise.failed(exc)
+      override def failed(e: Throwable, attachment: Void): Unit =
+        Promise.failed(e)
     })
 
     p.future
